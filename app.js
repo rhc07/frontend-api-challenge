ab2f9c9f9e4cce548c5b9d8ddf4f06326ca819a7
rhc07 <rorieclarke@gmail.com>
2021-03-20 17:27:01 +0000

    refactored the html

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ let listOfPeeps = document.getElementById("list-of-peeps");
         fetch('https://chitter-backend-api-v2.herokuapp.com/peeps')
         .then(res => res.json())
         .then(peeps => { 
-            for(let i = 0; i < 50; i++ ){
             peeps.forEach(peep =>  {
-                listOfPeeps.innerHTML += `<h3>Chitter Post</h3>
-                <p> ${peep.body} </p>
-                <h4> ${peep.user.handle} </h4>` 
+                listOfPeeps.innerHTML += `<h3>Chitter Post:</h3>
+                <p><i> "${peep.body}" </i></p>
+                <h6> @${peep.user.handle} </h6>` 
              }) 
-            }
         })
 
 
